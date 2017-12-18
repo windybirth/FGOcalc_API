2327801660bb5fca6f84432e622fdb783ff8bc74
windy <windybirth@gmail.com>
2017-12-19 01:18:03 +0900

    add expect to bash

--- release.sh
+++ release.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+#!/usr/bin/expect
+set timeout 30
+
 git pull
 gradle build
-sudo nohup java -jar build/libs/fgocalc-rest-service-0.1.0.jar >logs/SpringRuntimeLog.txt & 
+spawn sudo nohup java -jar build/libs/fgocalc-rest-service-0.1.0.jar >logs/SpringRuntimeLog.txt & 
+expect "password for"
+send "password\r"
+interact
