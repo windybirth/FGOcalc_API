1be2e9c6ad7d8a9d149a4c585f32e74a0596e4a1
windy <windybirth@gmail.com>
2017-12-19 01:39:03 +0900

    update test message

--- src/main/java/org/phantancy/fgocalc/api/control/ConfigInfoController.java
+++ src/main/java/org/phantancy/fgocalc/api/control/ConfigInfoController.java
@@ -20,7 +20,7 @@ public class ConfigInfoController {
     @RequestMapping("/test")
     public Response<ConfigInfo> test(@RequestParam(value="name", defaultValue="World") String name) {
     	ConfigInfo configInfo = new ConfigInfo("v1.5.1","http://nj005py.gitee.io/fgocalc/",
-        		"1更新数据至195号萝莉；2玄学计算器改欧非检测仪；3加入数据库单独更新功能；4其它优化");
+        		"This is test message.");
     	Response<ConfigInfo> response = new Response<ConfigInfo>("Success", null, configInfo);
         return response;
     }
