c21e3374b3ff33b19f7a8e804a1bc3534f67d692
windy <windybirth@gmail.com>
2017-12-18 23:59:03 +0900

    build with jetty

--- src/main/java/org/phantancy/fgocalc/api/Application.java
+++ src/main/java/org/phantancy/fgocalc/api/Application.java
@@ -2,10 +2,22 @@ package org.phantancy.fgocalc.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	/**
+     *重写configure
+     * @param builder
+     * @return
+     */
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(Application.class);
+//    }
 }
